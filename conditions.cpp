dd2cfa9f55e0393da72f253052fa4764770ea53c
Sushant Yadav <sushantmasthai@gmail.com>
2026-05-31 02:29:06 +0530

    conditions.cpp

--- conditions.cpp
+++ conditions.cpp
@@ -1,12 +1,35 @@
 // if statement
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int age = 20;
+
+//     if (age >= 18) {
+//         cout << "You can vote";
+//     }
+
+//     return 0;
+// }
+
+
+
+
+// 2. if-else Statement
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int age = 20;
+    int age ;
+    cout<<"Enter the age of person"<<endl;
+    cin>>age;
 
     if (age >= 18) {
         cout << "You can vote";
+    }
+    else {
+        cout << "You cannot vote";
     }
 
     return 0;
