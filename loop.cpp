05e2faae2ba510cbdbcb50a2e3704304c8306978
Sushant Yadav <sushantmasthai@gmail.com>
2026-05-31 11:25:39 +0530

    loop.cpp

---
+++ loop.cpp
@@ -0,0 +1,37 @@
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     for(int i=0;i<5;i=i+1){
+//         cout<<"hello sushant"<<endl;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main (){
+// for (int i=0; i<20; i=i+3){
+//     cout<<i<<endl;
+// }
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main (){
+// for (int i=20; i>0; i=i-3){
+//     cout<<i<<endl;
+// }
+// }
+
+
+
+#include<iostream>
+using namespace std;
+int main (){
+for (int i=0; i<10; i=i+1){
+    cout<<i*9<<endl;
+}
+}
