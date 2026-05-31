ff53912d29756f0060c4c82c78ca5db33f1c9e5a
Sushant Yadav <sushantmasthai@gmail.com>
2026-05-31 11:50:50 +0530

    arithmetic.cpp

--- loop.cpp
+++ loop.cpp
@@ -28,10 +28,25 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+// int main (){
+// for (int i=0; i<10; i=i+1){
+//     cout<<i*9<<endl;
+// }
+// }
+
+
+
 #include<iostream>
 using namespace std;
 int main (){
-for (int i=0; i<10; i=i+1){
-    cout<<i*9<<endl;
+for (int i=0; i<5; i=i+1){
+    cout<<"*"<<endl;
 }
+for (int i=5; i>0; i=i-1){
+    cout<<"*"<<endl;
+    
+}
+return 0;
 }
