6eea77ce8308ca95727331241bb736758dab6d79
Sushant Yadav <sushantmasthai@gmail.com>
2026-05-31 13:08:19 +0530

    pattern.cpp

---
+++ patter.cpp
@@ -0,0 +1,21 @@
+// print  solide rectangle
+      // *****
+      // *****
+      // *****
+
+      #include<iostream>
+      using namespace std;
+      int main (){
+
+        int i,j;
+        for(i=0; i<3; i=i+1) //row outer loop
+        {
+            for(j=0; j<5; j=j+1) //colum inner loop
+            {
+             cout<<" * ";
+        }
+        
+          cout<<endl;
+      }
+       return 0;
+    }
