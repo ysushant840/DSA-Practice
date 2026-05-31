ddabb78f3c84816f7998e8965f6a1467b41264b2
Sushant Yadav <sushantmasthai@gmail.com>
2026-05-31 13:17:40 +0530

    pattern.cpp

--- patter.cpp
+++ patter.cpp
@@ -3,19 +3,46 @@
       // *****
       // *****
 
-      #include<iostream>
-      using namespace std;
-      int main (){
+    //   #include<iostream>
+    //   using namespace std;
+    //   int main (){
 
-        int i,j;
-        for(i=0; i<3; i=i+1) //row outer loop
-        {
-            for(j=0; j<5; j=j+1) //colum inner loop
-            {
-             cout<<" * ";
-        }
+    //     int i,j;
+    //     for(i=0; i<3; i=i+1) //row outer loop
+    //     {
+    //         for(j=0; j<5; j=j+1) //colum inner loop
+    //         {
+    //          cout<<" * ";
+    //     }
         
-          cout<<endl;
-      }
-       return 0;
+    //       cout<<endl;
+    //   }
+    //    return 0;
+    // }
+
+
+
+
+    // print  Square Pattern
+      // * * * * *
+      // * * * * *
+    //   * * * * *
+    //   * * * * *
+    //   * * * * * *
+
+    #include<iostream>
+    using namespace std;
+    int main ()
+    {
+        int i,j;
+        for(i=0; i<5; i++)
+        {
+            for (j=0; j<5; j++)
+            {
+                cout<<" * ";
+            }
+            cout<<endl;
+
+        }
+        return 0;
     }
