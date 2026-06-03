95c4c1ed2419eeba17c580780aa3040438dc8fc4
Sushant Yadav <sushantmasthai@gmail.com>
2026-06-03 17:04:09 +0530

    function.cpp

--- funtion/function.cpp
+++ funtion/function.cpp
@@ -1,24 +1,47 @@
+// #include<iostream>
+// using namespace std;
+
+// void printname() {
+//     int n,i;
+//     cout<<"enter the number "<<endl;
+//     cin>>n;
+
+//     for(i=0;i<n;i++)
+//     {
+//         cout<<"sushant"<<endl;
+//     }
+
+// }
+
+// int main (){
+//     // function call
+//     printname();
+// }
+
+
+
+
+
+
+// Example 2: Addition Function
+
 #include<iostream>
 using namespace std;
 
-void printname() {
-    int n,i;
-    cout<<"enter the number "<<endl;
-    cin>>n;
+void addition()
+{
+    int a,b;
 
-    for(i=0;i<n;i++)
-    {
-        cout<<"sushant"<<endl;
-    }
+    cout<<"Enter value of a and b : ";
+    cin>>a>>b;
 
+    cout<<"Addition of a and b = "<<a+b;
 }
 
-int main (){
-    // function call
-    printname();
+int main()
+{
+    addition();
 }
-
-
 
 
 
