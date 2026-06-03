1aca807ca4dd9ddd67730319131816c4e3064844
Sushant Yadav <sushantmasthai@gmail.com>
2026-06-03 13:26:45 +0530

    function.cpp

---
+++ function.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void printname() {
+    int n,i;
+    cout<<"enter the number "<<endl;
+    cin>>n;
+
+    for(i=0;i<n;i++)
+    {
+        cout<<"sushant"<<endl;
+    }
+
+}
+
+int main (){
+    // function call
+    printname();
+}
