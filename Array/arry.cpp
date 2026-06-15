a827f4734c6e3de097039909194d57c19475a60c
Sushant Yadav <sushantmasthai@gmail.com>
2026-06-15 22:05:19 +0530

    arry.cpp

---
+++ Array/arry.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int arr[] = {1, 2, 3, 4, 5};
+
+int main()
+{
+    cout << "Array elements: ";
+
+    for(int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
