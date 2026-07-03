7c94c09c1bb9a13bc620737bfd6003035f94af58
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-03 22:15:22 +0530

    linearSearch.cpp

--- Array/linearSearch.cpp
+++ Array/linearSearch.cpp
@@ -1,32 +1,66 @@
+// without function code 
 #include<iostream>
 using namespace std;
-bool find(int arr[], int Size, int key){
-
-         for(int i=0;i<Size;i++){
-            if(arr[i]==key){
-                return true;
-            }
-        }
-        return false;
-}
-
-
-
-int main()
-{
+int main (){
     int arr[5]={10,20,30,40,50};
-    int Size=5;
     int key;
-
-    cout<<"Enter the key"<<endl;
     cin>>key;
 
-    if(find(arr,Size,key)){
-        cout<<"Found"<<endl;
+    bool found=false;
+    for(int i=0;i<5;i++){
+        if(arr[i]==key){
+            found=true;
+            break;
+        }
+    }
+    if (found) {
+        cout<<"element found";
     }
     else{
-        cout<<"Not Found"<<endl;
+        cout<<"Not found";
     }
-
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+// // line search 
+// #include<iostream>
+// using namespace std;
+// bool find(int arr[], int Size, int key){
+
+//          for(int i=0;i<Size;i++){
+//             if(arr[i]==key){
+//                 return true;
+//             }
+//         }
+//         return false;
+// }
+
+
+
+// int main()
+// {
+//     int arr[5]={10,20,30,40,50};
+//     int Size=5;
+//     int key;
+
+//     cout<<"Enter the key"<<endl;
+//     cin>>key;
+
+//     if(find(arr,Size,key)){
+//         cout<<"Found"<<endl;
+//     }
+//     else{
+//         cout<<"Not Found"<<endl;
+//     }
+
+//     return 0;
+// }
