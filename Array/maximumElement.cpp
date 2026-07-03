72927411b2605d08ab2c092236eff35f1e512c97
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-03 22:57:04 +0530

    maximumElement.cpp

--- Array/maximumElement.cpp
+++ Array/maximumElement.cpp
@@ -1,19 +1,44 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+
+//     int arr[5] = {10, 50, 30, 90, 40};
+
+//     int maximum = arr[0];
+
+//     for(int i = 1; i < 5; i++) {
+//         if(arr[i] > maximum) {
+//             maximum = arr[i];
+//         }
+//     }
+
+//     cout << "Maximum = " << maximum;
+
+//     return 0;
+// }
+
+
+// Reverse of an Array
+#include <iostream>
 using namespace std;
 
 int main() {
 
-    int arr[5] = {10, 50, 30, 90, 40};
+    int arr[5] = {10, 20, 30, 40, 50};
 
-    int maximum = arr[0];
+    int start = 0;
+    int end = 4;
 
-    for(int i = 1; i < 5; i++) {
-        if(arr[i] > maximum) {
-            maximum = arr[i];
-        }
+    while (start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
     }
 
-    cout << "Maximum = " << maximum;
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
