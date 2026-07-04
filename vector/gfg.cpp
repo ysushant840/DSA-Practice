1a5077048b80e3338e1c7367a5ea0dd8dd31f5ea
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-04 23:45:37 +0530

    gfg.cpp

--- vector/gfg.cpp
+++ vector/gfg.cpp
@@ -1,21 +1,39 @@
+// #include <iostream>
+// #include <string>
+// using namespace std;
+
+// class Solution {
+// public:
+//     string compareFive(int n) {
+//         if (n > 5)
+//             return "Greater than 5";
+//         else if (n < 5)
+//             return "Less than 5";
+//         else
+//             return "Equal to 5";
+//     }
+// };
+
+// int main() {
+//     Solution obj;
+//     cout << obj.compareFive(8);
+//     return 0;
+// }
+
+
+
 #include <iostream>
-#include <string>
 using namespace std;
 
-class Solution {
-public:
-    string compareFive(int n) {
-        if (n > 5)
-            return "Greater than 5";
-        else if (n < 5)
-            return "Less than 5";
-        else
-            return "Equal to 5";
-    }
-};
-
 int main() {
-    Solution obj;
-    cout << obj.compareFive(8);
+    int n;
+    cin >> n;
+
+    if(n%2==0){
+        cout<<"Friend"<<endl;
+    }
+else {
+    cout<<"You"<<endl;
+}
     return 0;
 }
