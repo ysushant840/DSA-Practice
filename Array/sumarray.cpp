a7ec036e15204c36d9dba64447629cbec34f4c82
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-08 02:46:23 +0530

    sumarray.cpp

--- Array/sumarray.cpp
+++ Array/sumarray.cpp
@@ -1,3 +1,37 @@
+// #include <iostream>
+// using namespace std;
+
+// int sumArray(int arr[], int n) {
+
+//     int sum = 0;
+
+//     for(int i = 0; i < n; i++) {
+//         sum += arr[i];
+//     }
+
+//     return sum;
+// }
+
+// int main() {
+
+//     int arr[5] = {10, 20, 30, 40, 50};
+
+//     cout << "Sum = " << sumArray(arr, 5);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -6,7 +40,7 @@ int sumArray(int arr[], int n) {
     int sum = 0;
 
     for(int i = 0; i < n; i++) {
-        sum += arr[i];
+        sum = sum + arr[i];
     }
 
     return sum;
