02d3c7c82c8c2611d9ac450d0d7b4190ea7d73af
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-07 23:10:15 +0530

    ncr.cpp

--- funtion file/ncr.cpp
+++ funtion file/ncr.cpp
@@ -1,11 +1,54 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// int factorial(int n)
+// {
+//     int fact = 1;
+
+//     for(int i=1; i<=n; i++)
+//     {
+//         fact = fact * i;
+//     }
+
+//     return fact;
+// }
+
+// int nCr(int n, int r)
+// {
+//     int ans = factorial(n) /
+//              (factorial(r) * factorial(n-r));
+
+//     return ans;
+// }
+
+// int main()
+// {
+//     int n,r;
+
+//     cout<<"Enter n and r : ";
+//     cin>>n>>r;
+
+//     cout<<"nCr = "<<nCr(n,r);
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+#include <iostream>
 using namespace std;
 
+// Function to calculate factorial
 int factorial(int n)
 {
     int fact = 1;
 
-    for(int i=1; i<=n; i++)
+    for (int i = 1; i <= n; i++)
     {
         fact = fact * i;
     }
@@ -13,22 +56,23 @@ int factorial(int n)
     return fact;
 }
 
+// Function to calculate nCr
 int nCr(int n, int r)
 {
-    int ans = factorial(n) /
-             (factorial(r) * factorial(n-r));
+    int num = factorial(n);
+    int den = factorial(r) * factorial(n - r);
+
+    int ans = num / den;
 
     return ans;
 }
 
 int main()
 {
-    int n,r;
+    int n, r;
+    cin >> n >> r;
 
-    cout<<"Enter n and r : ";
-    cin>>n>>r;
-
-    cout<<"nCr = "<<nCr(n,r);
+    cout << "nCr = " << nCr(n, r) << endl;
 
     return 0;
 }
