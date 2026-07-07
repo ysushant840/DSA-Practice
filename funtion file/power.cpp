32a5f649d81d9b3bfcdfb9a16294f2492828922e
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-07 21:33:03 +0530

    power.cpp

--- funtion file/power.cpp
+++ funtion file/power.cpp
@@ -7,12 +7,11 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    int ans = 1;
+    int ans1 = 1;
     for (int i = 1; i <= b; i++) {
-        ans = ans * a;
+        ans1 = ans1 * a;
     }
-
-    cout << "Answer is " << ans << endl;
+    cout << "Answer is " << ans1 << endl;
 
     // Second Power
     int c, d;
@@ -22,7 +21,6 @@ int main() {
     for (int i = 1; i <= d; i++) {
         ans2 = ans2 * c;
     }
-
     cout << "Answer is " << ans2 << endl;
 
     // Third Power
@@ -33,8 +31,17 @@ int main() {
     for (int i = 1; i <= f; i++) {
         ans3 = ans3 * e;
     }
-
     cout << "Answer is " << ans3 << endl;
+
+    // Fourth Power
+    int g, h;
+    cin >> g >> h;
+
+    int ans4 = 1;
+    for (int i = 1; i <= h; i++) {
+        ans4 = ans4 * g;
+    }
+    cout << "Answer is " << ans4 << endl;
 
     return 0;
 }
