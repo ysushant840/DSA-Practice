39a1e8ee0c9b00e2ca76b05051dba2047a77643f
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-07 21:23:14 +0530

    power.cpp

---
+++ funtion file/power.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a,b;
+    cin>>a>>b;
+
+    int ans =1;
+    for(int i=1;i<=b; i++){
+        ans=ans*a;
+    }
+    cout<<" ans is "<< ans << endl;
+    return 0;
+}
