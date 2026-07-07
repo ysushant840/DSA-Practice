fc13b357bb98f4bc0aa9a0c7062aa25f7da418ef
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-07 21:30:23 +0530

    power.ccpp

--- funtion file/power.cpp
+++ funtion file/power.cpp
@@ -1,25 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-    int a,b;
-    cin>>a>>b;
 
-    int ans =1;
-    for(int i=1;i<=b; i++){
-        ans=ans*a;
+int main() {
+
+    // First Power
+    int a, b;
+    cin >> a >> b;
+
+    int ans = 1;
+    for (int i = 1; i <= b; i++) {
+        ans = ans * a;
     }
-    cout<<" ans is "<< ans << endl;
-    return 0;
 
+    cout << "Answer is " << ans << endl;
 
+    // Second Power
+    int c, d;
+    cin >> c >> d;
 
-    int c,d;
-    cin>>c>>d;
-    int ams=1;
-    for(int i=1;i<=d; i++){
-        ans = ans *c;
-
+    int ans2 = 1;
+    for (int i = 1; i <= d; i++) {
+        ans2 = ans2 * c;
     }
-    cout<<" ans is "<< ans << endl;
+
+    cout << "Answer is " << ans2 << endl;
+
+    // Third Power
+    int e, f;
+    cin >> e >> f;
+
+    int ans3 = 1;
+    for (int i = 1; i <= f; i++) {
+        ans3 = ans3 * e;
+    }
+
+    cout << "Answer is " << ans3 << endl;
+
     return 0;
 }
