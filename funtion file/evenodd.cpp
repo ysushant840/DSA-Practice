eec0dacf159e376d900f7bb56111b65ef63a29e8
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-07 22:02:21 +0530

    evenodd.cpp

--- funtion file/evenodd.cpp
+++ funtion file/evenodd.cpp
@@ -1,18 +1,57 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+
+// // 1 -> even
+// // 0 -> odd
+
+// bool isEeven(int a)
+// {
+//     if(a & 1)
+//     {
+//         return 0;
+//     }
+//     else
+//     {
+//         return 1;
+//     }
+// }
+
+// int main()
+// {
+//     int num;
+//     cin >> num;
+
+//     if(isEeven(num))
+//     {
+//         cout << "No is even" << endl;
+//     }
+//     else
+//     {
+//         cout << "No is odd" << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+#include <iostream>
 using namespace std;
 
-// 1 -> even
-// 0 -> odd
+// 1 -> Even
+// 0 -> Odd
 
-bool isEeven(int a)
+bool isEven(int a)
 {
-    if(a & 1)
+    if (a % 2 == 0)
     {
-        return 0;
+        return true;
     }
     else
     {
-        return 1;
+        return false;
     }
 }
 
@@ -21,7 +60,7 @@ int main()
     int num;
     cin >> num;
 
-    if(isEeven(num))
+    if (isEven(num))
     {
         cout << "No is even" << endl;
     }
