bceed9f9d794180cd2cfb7e99190cde0f973cd1b
Sushant Yadav <sushantmasthai@gmail.com>
2026-07-09 08:44:16 +0530

    pratice.cpp

--- Array/pratice.cpp
+++ Array/pratice.cpp
@@ -1,86 +1,135 @@
+// #include <iostream>
+// using namespace std;
+
+// // Definition of TreeNode
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+
+//     TreeNode(int x) {
+//         val = x;
+//         left = NULL;
+//         right = NULL;
+//     }
+// };
+
+// class Solution {
+// public:
+//     TreeNode* first = NULL;
+//     TreeNode* second = NULL;
+//     TreeNode* prev = NULL;
+
+//     void inorder(TreeNode* root) {
+//         if (root == NULL)
+//             return;
+
+//         inorder(root->left);
+
+//         if (prev != NULL && prev->val > root->val) {
+//             if (first == NULL)
+//                 first = prev;
+//             second = root;
+//         }
+
+//         prev = root;
+
+//         inorder(root->right);
+//     }
+
+//     void recoverTree(TreeNode* root) {
+//         inorder(root);
+
+//         if (first != NULL && second != NULL)
+//             swap(first->val, second->val);
+//     }
+
+//     void printInorder(TreeNode* root) {
+//         if (root == NULL)
+//             return;
+
+//         printInorder(root->left);
+//         cout << root->val << " ";
+//         printInorder(root->right);
+//     }
+// };
+
+// int main() {
+
+//     // Example:
+//     //      3
+//     //     / \
+//     //    1   4
+//     //       /
+//     //      2
+//     //
+//     // Swapped nodes: 2 and 3
+
+//     TreeNode* root = new TreeNode(3);
+//     root->left = new TreeNode(1);
+//     root->right = new TreeNode(4);
+//     root->right->left = new TreeNode(2);
+
+//     Solution obj;
+
+//     cout << "Before Recovery (Inorder): ";
+//     obj.printInorder(root);
+//     cout << endl;
+
+//     obj.recoverTree(root);
+
+//     cout << "After Recovery (Inorder): ";
+//     obj.printInorder(root);
+//     cout << endl;
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-
-// Definition of TreeNode
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode(int x) {
-        val = x;
-        left = NULL;
-        right = NULL;
-    }
-};
 
 class Solution {
 public:
-    TreeNode* first = NULL;
-    TreeNode* second = NULL;
-    TreeNode* prev = NULL;
+    int maxCharGap(string &s) {
 
-    void inorder(TreeNode* root) {
-        if (root == NULL)
-            return;
+        vector<int> first(26, -1);
+        int ans = -1;
 
-        inorder(root->left);
+        for (int i = 0; i < s.length(); i++) {
 
-        if (prev != NULL && prev->val > root->val) {
-            if (first == NULL)
-                first = prev;
-            second = root;
+            int index = s[i] - 'a';
+
+            if (first[index] == -1) {
+                first[index] = i;
+            }
+            else {
+                ans = max(ans, i - first[index] - 1);
+            }
         }
 
-        prev = root;
-
-        inorder(root->right);
-    }
-
-    void recoverTree(TreeNode* root) {
-        inorder(root);
-
-        if (first != NULL && second != NULL)
-            swap(first->val, second->val);
-    }
-
-    void printInorder(TreeNode* root) {
-        if (root == NULL)
-            return;
-
-        printInorder(root->left);
-        cout << root->val << " ";
-        printInorder(root->right);
+        return ans;
     }
 };
 
 int main() {
 
-    // Example:
-    //      3
-    //     / \
-    //    1   4
-    //       /
-    //      2
-    //
-    // Swapped nodes: 2 and 3
-
-    TreeNode* root = new TreeNode(3);
-    root->left = new TreeNode(1);
-    root->right = new TreeNode(4);
-    root->right->left = new TreeNode(2);
+    string s;
+    cin >> s;
 
     Solution obj;
 
-    cout << "Before Recovery (Inorder): ";
-    obj.printInorder(root);
-    cout << endl;
-
-    obj.recoverTree(root);
-
-    cout << "After Recovery (Inorder): ";
-    obj.printInorder(root);
-    cout << endl;
+    cout << obj.maxCharGap(s) << endl;
 
     return 0;
 }
